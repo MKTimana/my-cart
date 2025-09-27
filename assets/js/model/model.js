f39d01c0f0d526876a4a4aeb8475b9881221be65
Milton Timana <miltonkevin10@gmail.com>
2025-09-27 19:51:29 +0200

    metodos do carrinho criados no model.js

---
+++ assets/js/model/model.js
@@ -0,0 +1,67 @@
+window.Model = {
+    _id: 1,
+    productos: [], // aqui se espera { id, produto, preço}
+    carrinho: [], // aqui se espera { id, carrinho, preço, qty}
+
+    //metodo Adicionar producto
+    adicionarProduto(name, price) {
+        const prod = {
+            id: this._id++,
+            name,
+            price: Number(price)
+        };
+        this.productos.push(p);
+        return p;
+    },
+
+    // Metodo listar produtos existentes
+    listarProduto() {
+        return [...this.productos];
+    },
+
+    // Metodo para adicionar ao carrinho
+    adicionarAoCarrinho(prodId) {
+        const p = this.productos.find(x => x.id === prodId)
+        if (!p) return;
+        const linhaCarrinho = this.carrinho.find(x => x.id === prodId);
+        if (linhaCarrinho) {
+            // caso o item já esteja no carrinho, somente fará o incremento do mesmo
+            linhaCarrinho.qty += 1;
+        } else {
+            // cc irá adicionar
+            this.carrinho.push({
+                id: p.id,
+                name: p.name,
+                price: p.price,
+                qty: 1
+            })
+        }
+    },
+
+    // Apagar item do carrinho
+    removerDoCarrinho(prodId) {
+        const item = this.carrinho.findIndex(x => x.id === prodId)
+        if (item >= 0) this.carrinho.splice(item, 1);
+    },
+
+    limparCarrinho() {
+        this.carrinho = [];
+    },
+
+    // Trocar a qty de itens existentes.
+    mudarQty(prodId, q) {
+        const item = this.carrinho.find(x => x.id === prodId);
+        if (!item) return;
+        item.qty += devicePixelRatio;
+        if (item.qty <= 0) this.removerDoCarrinho(prodId)
+    },
+
+    totalCarrinho() {
+        const quantidade = this.carrinho.reduce((s, item) => s + item.qty, 0);
+        const total = this.carrinho.reduce((s, item) => s + item.qty * item.price, 0);
+        return {
+            quantidade, total
+        }
+    }
+
+}
